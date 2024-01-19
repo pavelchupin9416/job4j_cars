1708f6d4136eb156a1e3ecc7880752f3477c85d6
pavel.chupin <pavelchupin94@yandex.ru>
2024-01-19 10:04:03 +0300

    1. Фильтры для объявлений машин. Фикс.

--- src/main/java/ru/job4j/cars/model/Post.java
+++ src/main/java/ru/job4j/cars/model/Post.java
@@ -6,6 +6,7 @@ import lombok.EqualsAndHashCode;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -46,6 +47,6 @@ public class Post {
 
     @OneToMany(fetch = FetchType.LAZY)
     @JoinColumn(name = "file_id")
-    private Set<File> files;
+    private Set<File> files = new HashSet<>();
 
 }
